0e20d3214b0f9954bd3f8a0f41d005f4c80afed0
Bianca <bianca.ionashiro@gmail.com>
2024-01-25 19:30:58 -0300

    WIP - Desafio 1 e alguns ajustes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,8 +2,13 @@ function Card() {
   return (
     <>
       <div className="card">
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+        <div className="container">
+          <h2>Rick Sanchez</h2>
+          <h5>Status: Vivo</h5>
+          <h5>Espécie: Humana</h5>
+          <h5>Origem: Terra C-137</h5>
+          <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+        </div>
       </div>
       <div className="card">
         <h2>Morty Smith</h2>
