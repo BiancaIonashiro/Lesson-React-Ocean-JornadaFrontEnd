1ddf4143f856a840760fe1e84e3753b3a3c3149d
Bianca <bianca.ionashiro@gmail.com>
2024-01-30 10:41:29 -0300

    WIP - Criação de novos personagens por propriedades

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,18 @@
-function Card() {
+function Card(props) {
+  //console.log(props);
+  const item = props.item;
+  //console.log(item);
+
   return (
     <>
       <div className="card">
         <div className="container">
-          <h2>Rick Sanchez</h2>
+          <h2>{item.name}</h2>
           <h5>Status: Vivo</h5>
           <h5>Espécie: Humana</h5>
           <h5>Origem: Terra C-137</h5>
-          <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+          <img src={item.image} />
         </div>
-      </div>
-      <div className="card">
-        <h2>Morty Smith</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" />
-      </div>
-      <div className="card">
-        <h2>Summer Smith</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/3.jpeg" />
-      </div>
-      <div className="card">
-        <h2>Beth Smith</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/4.jpeg" />
       </div>
     </>
   );
