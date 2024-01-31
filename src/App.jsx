63ceddbaf8c0dab523e0a8c77719c2caa20c321b
Bianca <bianca.ionashiro@gmail.com>
2024-01-31 11:57:07 -0300

    WIP - Isolando o carregamento através do hook useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card/Card";
 
 function App() {
   //Carregamento de dados via código
+  // const item1 = {
+  //   name: "Rick Sanchez",
+  //   image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  // };
+  // const item2 = {
+  //   name: "Morty Smith",
+  //   image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+  // };
+  // const item3 = {
+  //   name: "Summer Smith",
+  //   image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+  // };
+  // const item4 = {
+  //   name: "Beth Smith",
+  //   image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+  // };
 
-  const item1 = {
-    name: "Rick Sanchez",
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-  };
-  const item2 = {
-    name: "Morty Smith",
-    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-  };
-  const item3 = {
-    name: "Summer Smith",
-    image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-  };
-  const item4 = {
-    name: "Beth Smith",
-    image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
-  };
+  // const itens = [item1, item2, item3, item4];
 
-  const itens = [item1, item2, item3, item4];
+  const [itens, setItens] = useState([]);
 
   //Carregamento de dados via API(backend)
   async function carregarDadosApi() {
@@ -31,17 +33,24 @@ function App() {
     const response = await fetch(apiURL);
 
     //console.log(response);
-
     const body = await response.json();
+
+    //console.log(body);
 
     //Extrair a propriedade results do body
     //Essa propriedade contém a lista de itens
+    const results = body.results;
 
-    console.log(body);
+    console.log(results);
+
+    //Atualiza o estado "itens" com o resultado da API
+    setItens(results);
   }
 
-  //Chamando a função que carrega os dados da API
-  carregarDadosApi();
+  useEffect(function () {
+    //Chamando a função que carrega os dados da API
+    carregarDadosApi();
+  }, []);
 
   return (
     <>
